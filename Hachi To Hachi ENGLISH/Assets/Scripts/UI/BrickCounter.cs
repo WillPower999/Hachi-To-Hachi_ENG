efa957a200974dd17990697024552f3580a88138
Beck <69282901+plumfumble@users.noreply.github.com>
2021-09-09 20:33:42 -0500

    HUD STUFF (with brick!)
    i forgot the brick :(

---
+++ Hachi To Hachi ENGLISH/Assets/Scripts/UI/BrickCounter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickCounter : MonoBehaviour
+{
+
+    public int brickcount;
+
+
+    public void gainBrick()
+    {
+        brickcount += 1;
+        
+    }
+}

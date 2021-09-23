3d07f3cebf8956de87316283ecd6056bad21382f
Beck <69282901+plumfumble@users.noreply.github.com>
2021-09-23 17:46:25 -0500

    Added Saves
    I hope this works.

---
+++ Hachi To Hachi ENGLISH/Assets/Scripts/Data/GameSave.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+
+public class GameSave 
+{
+    public int maxhealth;
+    public int brickcount;
+    public Vector3 location;
+}
--- Hachi To Hachi ENGLISH/Assets/Scripts/UI/HUD/HealthBar.cs
+++ Hachi To Hachi ENGLISH/Assets/Scripts/UI/HUD/HealthBar.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     const int MAXPOSSHP = 6; //This way, we can change the max possible HP without having to change the value everywhere we need it
-    
+
+    private const int EMPTY = 0;
+    private const int FULL = 1;
+
     public Image[] healthPoints;
     public Sprite[] healthStatus;
     // 0 = empty, 1 = full
@@ -21,16 +24,20 @@ public class HealthBar : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void UpdateUI()
     {
         health = playerHealth.health;
         maxhealth = playerHealth.maxhealth;
 
-        for (int index = 0; index <= MAXPOSSHP; index++)
+        for (int index = 0; index < MAXPOSSHP; index++)
         {
+            healthPoints[index].sprite = index < health ? healthStatus[FULL] : healthStatus[EMPTY];
+            healthPoints[index].gameObject.SetActive(index < maxhealth);
+            /*
             if (index + 1 < health) healthPoints[index].sprite = healthStatus[1];
             else if (index + 1 > maxhealth) healthPoints[index].gameObject.SetActive(false);
             else healthPoints[index].sprite = healthStatus[0];
+            */
         }
     }
 }

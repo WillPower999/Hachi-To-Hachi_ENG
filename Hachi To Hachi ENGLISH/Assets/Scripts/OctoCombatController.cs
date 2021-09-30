b5d04fc3088e3b76d2f0f24747accf0f40fd9c6f
WEBSTERAD\WUStudent <dts@webster.edu>
2021-09-30 15:22:30 -0500

    Projectile controls added

--- Hachi To Hachi ENGLISH/Assets/Scripts/OctoCombatController.cs
+++ Hachi To Hachi ENGLISH/Assets/Scripts/OctoCombatController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class OctoCombatController : MonoBehaviour
 {
+    public GameObject inkShot;
+    public float velocity = 20f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class OctoCombatController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            GameObject projectile = Instantiate(inkShot, transform.position + new Vector3(0f,1f,0f),
+                                                      transform.rotation);
+            projectile.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * velocity);
+        }
     }
 }

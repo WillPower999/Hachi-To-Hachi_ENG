2613edafe3d47895dd9fd25ee4c911356e3fb744
WEBSTERAD\WUStudent <dts@webster.edu>
2021-10-03 18:31:39 -0500

    Mode updates

--- Hachi To Hachi ENGLISH/Assets/Scripts/BeeCombatController.cs
+++ Hachi To Hachi ENGLISH/Assets/Scripts/BeeCombatController.cs
@@ -2,25 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BeeCombatController : MonoBehaviour
+namespace MenteBacata.ScivoloCharacterControllerDemo
 {
-    public GameObject honeyDrop;
-    public float velocity = 20f;
-
-    // Start is called before the first frame update
-    void Start()
+    public class BeeCombatController : MonoBehaviour
     {
-        
-    }
+        public GameObject honeyDrop;
+        public GameObject movement;
+        public float velocity = 20f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(1))
+        // Start is called before the first frame update
+        void Start()
         {
-            GameObject projectile = Instantiate(honeyDrop, transform.position + new Vector3(0f, .5f, 0f),
-                                                      transform.rotation);
-            projectile.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * velocity);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            string mode = movement.GetComponent<MovementManager>().movementMode;
+
+            if (Input.GetMouseButtonDown(1) && mode != "Sea")
+            {
+                GameObject projectile = Instantiate(honeyDrop, transform.position + new Vector3(0f, .5f, 0f),
+                                                          transform.rotation);
+                projectile.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * velocity);
+            }
         }
     }
 }
--- Hachi To Hachi ENGLISH/Assets/Scripts/OctoCombatController.cs
+++ Hachi To Hachi ENGLISH/Assets/Scripts/OctoCombatController.cs
@@ -2,39 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OctoCombatController : MonoBehaviour
+namespace MenteBacata.ScivoloCharacterControllerDemo
 {
-    public GameObject inkShot;
-    public float velocity = 20f;
-
-    public GameObject meleeBox;
-    
-    // Start is called before the first frame update
-    void Start()
+    public class OctoCombatController : MonoBehaviour
     {
-        
-    }
+        public GameObject inkShot;
+        public GameObject movement;
+        public float velocity = 20f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(1))
+        public GameObject meleeBox;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            GameObject projectile = Instantiate(inkShot, transform.position + new Vector3(0f,1f,0f),
-                                                      transform.rotation);
-            projectile.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * velocity);
+
         }
 
-        if (Input.GetMouseButtonDown(0))
+        // Update is called once per frame
+        void Update()
         {
-            StartCoroutine(hitTime());
-        }
-    }
+            string mode = movement.GetComponent<MovementManager>().movementMode;
 
-    IEnumerator hitTime()
-    {
-        meleeBox.SetActive(true);
-        yield return new WaitForSeconds(0.1f);
-        meleeBox.SetActive(false);
+            if (Input.GetMouseButtonDown(1) && mode != "Sky")
+            {
+                GameObject projectile = Instantiate(inkShot, transform.position + new Vector3(0f, 1f, 0f),
+                                                          transform.rotation);
+                projectile.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * velocity);
+            }
+
+            if (Input.GetMouseButtonDown(0) && mode != "Sky")
+            {
+                StartCoroutine(hitTime());
+            }
+        }
+
+        IEnumerator hitTime()
+        {
+            meleeBox.SetActive(true);
+            yield return new WaitForSeconds(0.1f);
+            meleeBox.SetActive(false);
+        }
     }
 }

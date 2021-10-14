12709c9a29d9d87060de224a81feec5fc7c6436d
WEBSTERAD\WUStudent <dts@webster.edu>
2021-10-14 18:47:09 -0500

    Revert "Merge pull request #3 from WillPower999/jackson-enemy-coding"
    This reverts commit c4197bdeaa04ebba1d7c884580d351e2bd4a8436, reversing
    changes made to 085c2299af7362a31f6cdcdd64afda2343d053ab.

--- Hachi To Hachi ENGLISH/Assets/Scripts/UI/HUD/dealDamage.cs
+++ Hachi To Hachi ENGLISH/Assets/Scripts/UI/HUD/dealDamage.cs
@@ -9,13 +9,4 @@ public class dealDamage : MonoBehaviour
         PlayerHealth playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
         playerStats.TakeDamage(dam);
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            GlobalGameState.currentGame.health--;
-            Debug.Log(GlobalGameState.currentGame.health);
-        }
-    }
 }

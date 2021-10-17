379cc1a55b28b0c53d18766b8d9db6c3732f0ef9
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2021-10-16 23:40:24 -0500

    collectibles are in

---
+++ Hachi To Hachi ENGLISH/Assets/Collectibles/Code/DaifukuManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DaifukuManager : MonoBehaviour
+{
+    public static DaifukuManager Instance;
+
+    private int _totalCoinsInLevel;
+    private int _collectedCoinsInLevel;
+    private int _totalCoinsInGame;
+
+    public TextMeshProUGUI coinCount;
+    public TextMeshProUGUI coinCountTwo;
+
+    public AudioSource collectSound;
+
+    private void Awake()
+    {
+        Instance = this;
+        _totalCoinsInLevel = FindObjectsOfType<Daifuku>().Length;
+        DisplayCoinCount();
+
+        _totalCoinsInGame = PlayerPrefs.GetInt("coinCount");
+    }
+
+    void DisplayCoinCount()
+    {
+        //coinCount.SetText(_collectedCoinsInLevel + "/" + _totalCoinsInLevel);
+        //coinCountTwo.SetText(_collectedCoinsInLevel + "/" + _totalCoinsInLevel);
+
+        //coinCount.SetText(_collectedCoinsInLevel + "/" + _totalCoinsInLevel);
+        //coinCountTwo.SetText(_collectedCoinsInLevel + "/" + _totalCoinsInLevel);
+    }
+
+    public void IncrementCollectedCoinCount()
+    {
+        _collectedCoinsInLevel++;
+        DisplayCoinCount();
+
+        PlayerPrefs.SetInt("coinCount", PlayerPrefs.GetInt("coinCount") + 1);
+
+        /*if (_collectedCoinsInLevel >= _totalCoinsInLevel)
+        {
+            //You Win Menu? Or Level Continue
+            YouWinMenu.Instance.Show("You Win!");
+        }*/
+    }
+}

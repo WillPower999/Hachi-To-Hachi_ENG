e4e1abcab5cfb8e5e52b1a0b22f21fe1334d17a4
WillPower999 <williamcordelllegensworm@gmail.com>
2021-10-28 20:04:25 -0500

    did the day and the night a do

---
+++ Hachi To Hachi ENGLISH/Assets/Scripts/DayNightManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteAlways]
+public class DayNightManager : MonoBehaviour
+{
+    public Gradient AmbientColor;
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+    [SerializeField] public float slownessMultiplier;
+    [SerializeField] private float Dawn;
+    [SerializeField] private float Dusk;
+    [SerializeField] private float NightFastForwardInSeconds;
+
+    private void Update()
+    {
+        if (Application.isPlaying)
+        {
+            //(Replace with a reference to the game time)
+            TimeOfDay += Time.deltaTime * (1/slownessMultiplier);
+            TimeOfDay %= 24; //Modulus to ensure always between 0-24
+            UpdateLighting(TimeOfDay / 24f);
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 24f);
+        }
+        
+        if(TimeOfDay < Dawn || TimeOfDay > Dusk)
+        {
+            StartCoroutine(NightSkip());
+        }
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = AmbientColor.Evaluate(timePercent);
+
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+            return;
+    }
+
+    private IEnumerator NightSkip()
+    {
+        yield return new WaitForSeconds(NightFastForwardInSeconds);
+        TimeOfDay = Dawn;
+    }
+    
+
+}
